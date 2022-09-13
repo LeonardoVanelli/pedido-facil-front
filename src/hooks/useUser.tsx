0ee52611bb6de0349d9ee42d2766ca64da6e120a
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2022-09-13 20:39:53 -0300

    save user session in local storage

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 interface TransactionsProviderProps {
   children: React.ReactNode
@@ -12,25 +12,41 @@ interface IUser {
 interface UserContextData {
   user: IUser | null
   login: (user: IUser) => void
+  logout: () => void
 }
 
 const UserContext = createContext<UserContextData>({} as UserContextData)
 
-export function UserProvider ({ children }: TransactionsProviderProps) {
+const USER_STORAGE_KEY = "user"
+
+export function UserProvider({ children }: TransactionsProviderProps) {
   const [user, setUser] = useState<IUser | null>(null)
 
+  useEffect(() => {
+    const userStorage = localStorage.getItem(USER_STORAGE_KEY)
+    if (userStorage) {
+      setUser(JSON.parse(userStorage))
+    }
+  }, [])
+
   const login = (user: IUser) => {
+    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user))
     setUser(user)
   }
 
+  const logout = () => {
+    localStorage.removeItem(USER_STORAGE_KEY)
+    setUser(null)
+  }
+
   return (
-    <UserContext.Provider value={{ user, login }}>
+    <UserContext.Provider value={{ user, login, logout }}>
       {children}
     </UserContext.Provider>
   )
 }
 
-export function useUser () {
+export function useUser() {
   const context = useContext(UserContext)
 
   return context
